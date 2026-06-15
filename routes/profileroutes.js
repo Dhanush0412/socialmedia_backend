7e0d6075035f905c2f46db79b5561702fdcf8f6d
rdhanush <rdhanush329@gmail.com>
2026-06-15 17:32:20 +0530

    changes in profileroutes,userroutes

--- routes/profileroutes.js
+++ routes/profileroutes.js
@@ -3,7 +3,6 @@ let upload= require("../config/multer")
 let router = express.Router();
 
 let {createprofile,getdashboard, connectionprofile,updatedprofilepic} = require("../controllers/profilecontroller");
-const upload = require("../config/multer");
 
 router.get("/:profileid",getdashboard);
 router.post("/:profileid/:friendid",connectionprofile)
