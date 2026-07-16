399000ea9ea23093bc792a23a7e00bc586a8cecd
rdhanush <rdhanush329@gmail.com>
2026-07-16 16:04:27 +0530

    space changes

--- socket/socket.js
+++ socket/socket.js
@@ -3,39 +3,21 @@ let io;
 module.exports = {
 
     init: (Socketio) => {
-
         io = Socketio;
-
         io.on("connection", (socket) => {
-
             console.log("Connected:", socket.id);
-
             socket.on("register", (profileid) => {
-
                 socket.join(profileid);
-
                 console.log("Joined profile:", profileid);
-
             });
-
             socket.on("joingroup", (groupid) => {
-
                 socket.join(groupid);
-
                 console.log("Joined group:", groupid);
-
             });
-
             socket.on("disconnect", () => {
-
                 console.log("Disconnected:", socket.id);
-
             });
-
         });
-
     },
-
     getIO: () => io
-
 };
